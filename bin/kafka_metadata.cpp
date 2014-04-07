2bbb84877fa51cc4e6737e91d9e3ba67254c4a04
Fedor Korotkiy <prime@yandex-team.ru>
2014-04-07 14:19:27 +0400

    get rid of boost program options

--- bin/kafka_metadata.cpp
+++ bin/kafka_metadata.cpp
@@ -1,58 +1,34 @@
 #include <iostream>
 
-#include <boost/program_options.hpp>
+#include <gflags/gflags.h>
 
 #include <raptor/kafka/rt_network.h>
 #include <raptor/kafka/rt_link.h>
+#include <raptor/daemon/daemon.h>
 
-namespace po = boost::program_options;
-
-bool parse_options(int argc, char* argv[], po::variables_map* map) {
-	po::options_description desc("fetch metadata from kafka broker");
-	desc.add_options()
-		("help", "show this help message")
-		("host", po::value<std::string>(), "broker host")
-		("port", po::value<int>()->default_value(9092), "broker port")
-	;
-
-	po::store(po::parse_command_line(argc, argv, desc), *map);
-	po::notify(*map);
-
-	if(map->count("help")) {
-		std::cerr << desc << std::endl;
-		return false;
-	}
-
-	return true;
-}
-
-
+DEFINE_string(host, "localhost", "kafka host");
+DEFINE_int32(port, 9092, "kafka port");
 
 using namespace raptor;
+using namespace raptor::kafka;
 
 int main(int argc, char* argv[]) {
-	po::variables_map options;
-	if(!parse_options(argc, argv, &options)) return -1;
+	google::ParseCommandLineFlags(&argc, &argv, true);
+	setup_raptor();
 
 	scheduler_t scheduler;
 
-	kafka::options_t opts;
+	options_t opts;
 
-	kafka::rt_link_t link(
-		kafka::connect_socket(options["host"].as<std::string>(), options["port"].as<int>()),
-		opts
-	);
+	rt_link_t link(connect_socket(FLAGS_host, FLAGS_port), opts);
 
-	auto request = std::make_shared<kafka::metadata_request_t>();
-	auto response = std::make_shared<kafka::metadata_response_t>();
+	auto request = std::make_shared<metadata_request_t>();
+	auto response = std::make_shared<metadata_response_t>();
 
 	link.start(&scheduler);
 	link.send(request, response).get();
 
 	std::cout << *response;
-
-	link.close();
-	scheduler.shutdown();
 
 	return 0;
 }
